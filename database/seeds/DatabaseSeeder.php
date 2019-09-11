32628731188db5b43ac96229d074ef9231adf374
mmilos028 <mmilos028@gmail.com>
2019-09-11 12:17:43 +0200

    Prepopulate Users and Questions tables using model factories and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
         // $this->call(UsersTableSeeder::class);
+        //factory(App\User::class, 3)->create()->
+        factory(App\User::class, 3)->create()->each( function(App\User $u, $key){
+            /**
+             * @var $u App\User
+             */
+            $u->questions()
+                ->saveMany(
+                    factory(App\Question::class, rand(1, 5))->make()
+                    );
+            
+        });
+        
+        
     }
 }
