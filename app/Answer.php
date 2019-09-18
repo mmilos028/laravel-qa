cdc151ae60d30936d54ba06cea69b07f207efc49
mmilos028 <mmilos028@gmail.com>
2019-09-18 11:58:19 +0200

    Answers count to reflect actual number of answers per question

--- app/Answer.php
+++ app/Answer.php
@@ -22,4 +22,21 @@ class Answer extends Model
         return \Parsedown::instance()->text($this->body);
     }
     
+    public static function boot()
+    {
+        parent::boot();
+        
+        static::created(function($answer){
+            //echo "Answer created\n";
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+        
+        /*
+        static::saved(function($answer){
+            echo "Answer saved\n";
+        });
+        */
+    }
+    
 }
