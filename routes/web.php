bea6e1eebf70cb21995e55c314ce057985c871a4
mmilos028 <mmilos028@gmail.com>
2019-09-18 09:50:50 +0200

    Question details added

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('questions', 'QuestionsController');
+Route::resource('questions', 'QuestionsController')->except('show');
 
-Auth::routes();
+Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
