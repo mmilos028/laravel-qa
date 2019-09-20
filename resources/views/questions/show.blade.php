7a73594695d45743d9dfcaddb5d812809ec8f473
mmilos028 <mmilos028@gmail.com>
2019-09-20 13:16:32 +0200

    Vote controlls implemented to question and to answer

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -6,34 +6,52 @@
         
         <div class="col-md-12">
         	<div class="card">
-        		<div class="card-header">
-        			<div class="d-flex align-items-center">
-        				<h1>{{ $question->title }}</h1>
-        				<div class="ml-auto">
-        					<a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
-        						Back to all Questions
-        					</a>
-        				</div>
-        			</div>        			
-        		</div>
-        		
         		<div class="card-body">
-        			        			
-        			{!! $question->body_html !!}
-        			<div class="float-right">
-    					<span class="text-muted">Answered {{ $question->created_date }}</span>
-    					<div class="media mt-2">
-    						<a href="{{ $question->user->url }}" class="pr-2>
-    							<img src="{{ $question->user->avatar }}">
-    						</a>
-    						<div class="media-body mt-1">
-    							<a href="{{ $question->user->url }}">
-    								{{ $question->user->name }}
-    							</a>
-    						</div>        								
-    					</div>
-    				</div>
-        			
+            		<div class="card-title">
+            			<div class="d-flex align-items-center">
+            				<h1>{{ $question->title }}</h1>
+            				<div class="ml-auto">
+            					<a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
+            						Back to all Questions
+            					</a>
+            				</div>
+            			</div>        			
+            		</div>
+            		
+            		<hr>
+            		
+            		<div class="media">
+            			<div class="d-flex flex-column vote-controls">
+            				<a title="This question is useful" class="vote-up">
+            					<i class="fas fa-caret-up fa-3x"></i>
+            				</a>
+            				<span class="votes-count">123</span>
+            				<a title="This question is not useful" class="vote-down off">
+            					<i class="fas fa-caret-down fa-3x"></i>
+            				</a>
+            				<a title="Click to mark as favourite question (Click again to undo)" class="favourite mt-2 favourited">
+            					<i class="fas fa-star fa-2x"></i>
+            					<span class="favourites-count">123</span>
+            				</a>
+            			</div>
+            			<div class="media-body">        			
+                			{!! $question->body_html !!}
+                			<div class="float-right">
+            					<span class="text-muted">Answered {{ $question->created_date }}</span>
+            					<div class="media mt-2">
+            						<a href="{{ $question->user->url }}" class="pr-2>
+            							<img src="{{ $question->user->avatar }}">
+            						</a>
+            						<div class="media-body mt-1">
+            							<a href="{{ $question->user->url }}">
+            								{{ $question->user->name }}
+            							</a>
+            						</div>        								
+            					</div>
+            				</div>
+        				</div>
+            			
+        			</div>
     			</div>
     		</div>
     	</div>
@@ -49,7 +67,19 @@
         			<hr>
         			@foreach($question->answers as $answer)
         				<div class="media">
-        					<div class="media-body">
+        					<div class="d-flex flex-column vote-controls">
+                				<a title="This anwser is useful" class="vote-up">
+                					<i class="fas fa-caret-up fa-3x"></i>
+                				</a>
+                				<span class="votes-count">123</span>
+                				<a title="This anwser is not useful" class="vote-down off">
+                					<i class="fas fa-caret-down fa-3x"></i>
+                				</a>
+                				<a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                					<i class="fas fa-check fa-2x"></i>
+                				</a>
+                			</div>
+        					<div class="media-body">        						
         						{!! $answer->body_html !!}
         						<div class="float-right">
         							<span class="text-muted">Answered {{ $answer->created_date }}</span>
